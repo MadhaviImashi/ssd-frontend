3bbcfc9335055595489d50fda695ae371e919852
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-10 06:42:09 +0530

    refactored

--- src/API/Auth/auth.js
+++ src/API/Auth/auth.js
@@ -1,30 +1,3 @@
-// import http from "./base";
-
-// /**
-//  * @description: signup
-//  * @param {type}
-//  * @return:
-//  */
-// const SignUp = (data)=>{
-//     return http.post("/signup",data);
-// }
-
-// /*
-//  * @description: login
-//  * @param {type}
-//  */
-// const SignIn = (data)=>{
-//     return http.post("/login",data)
-// }
-
-
-// const all ={
-//     SignUp,
-//     SignIn
-// }
-
-// export default all;
-
 import { postRequest } from "../util";
 
 const BASE_URL = "http://localhost:4000";
