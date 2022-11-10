289e5777b2e3f14e3f8e1118b631fce02d7794f3
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-10 06:41:24 +0530

    home pages for each user role

---
+++ src/components/pages/managers/ManagerHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ManagerHome = () => {
+
+    
+  return (
+    <>
+      <div>Manager's page</div>
+    </>
+  );
+};
+
+export default ManagerHome;
---
+++ src/components/pages/workers/WorkerHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WorkerHome = () => {
+
+    
+  return (
+    <>
+      <div>Workers page</div>
+    </>
+  );
+};
+
+export default WorkerHome;
