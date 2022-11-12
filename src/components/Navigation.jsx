aaadd746c599667bc958ba19e6abbf0910acfa43
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-12 21:03:56 +0530

    common header component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,13 @@ import React, {Component} from 'react';
 class Navigation extends Component {
     render() {
         return (
+            <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{ backgroundColor: "black", minWidth: "100%", position: "fixed"}}>
                 <div className="container-fluid" style={{marginLeft: "20px", color: "white", backgroundColor: "black"}}>
-                    <div className="navbar-brand" to="/" style={{ padding: "20px", fontSize: "20px", fontWeight: "400"}}>ABC Company</div>
+                    <div className="navbar-brand" to="/" style={{ padding: "20px", fontSize: "20px", fontWeight: "400"}}>🚀 ABC Company</div>
                 </div>
-            </nav>
+                </nav><br /><br /><br />
+            </>
         );
     }
 }
