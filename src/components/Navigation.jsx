d0072a282de222aa70f1a91bb796fc7b5ba6f153
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-12 23:48:19 +0530

    app header updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ class Navigation extends Component {
     render() {
         return (
             <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{ backgroundColor: "black", minWidth: "100%", position: "fixed"}}>
+            <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "black", minWidth: "100%", position: "fixed"}}>
                 <div className="container-fluid" style={{marginLeft: "20px", color: "white", backgroundColor: "black"}}>
                     <div className="navbar-brand" to="/" style={{ padding: "20px", fontSize: "20px", fontWeight: "400"}}>🚀 ABC Company</div>
                 </div>
