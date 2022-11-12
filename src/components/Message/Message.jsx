943d1be4318b57838a0ad8ca88e63a25be50c712
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-12 21:03:36 +0530

    msg box

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,28 @@
+import { Button, Modal } from "react-bootstrap";
+const Message = (props) => {
+  
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        
+        {props.message}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default Message;
