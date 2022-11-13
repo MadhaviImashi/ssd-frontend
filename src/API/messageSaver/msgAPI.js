4cf7302854b51ac38838089f6c74eeb1d71fdf56
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-13 14:29:12 +0530

    API to get msg history

--- src/API/messageSaver/msgAPI.js
+++ src/API/messageSaver/msgAPI.js
@@ -1,8 +1,8 @@
-import { getByIDRequest, postRequest } from "../util";
+import { getRequest, postRequest } from "../util";
 
 const BASE_URL = "https://localhost:4000";
 
-export const getMessageHistory = (uri, id) => getByIDRequest(BASE_URL, uri, id);
+export const getMessageHistory = (uri, id) => getRequest(BASE_URL, uri, id);
 
 export const saveMessages = (uri, data) => postRequest(BASE_URL, uri, data);
 
