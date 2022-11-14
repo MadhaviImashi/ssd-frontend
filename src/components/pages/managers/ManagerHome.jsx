09fe280de9183f4d7e3211646b6091624dce10f9
MadhaviImashi <madhaviimashi@gmail.com>
2022-11-14 23:40:40 +0530

    add google signin in manager's page

--- src/components/pages/managers/ManagerHome.jsx
+++ src/components/pages/managers/ManagerHome.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { Button } from "@mui/material";
 import Navigation from "../../Navigation";
 import SendMessageBox from "../../sendMessageBox";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import UploadDrive from "../UploadDrive";
 
 const ManagerHome = () => {
 
   const Navigate = useNavigate();
+  
+  let clientId = process.env.REACT_APP_CLIENT_ID;
+  let redirectUrl = process.env.REACT_APP_REDIRECT_URL;
+  let scope = process.env.REACT_APP_SCOPE;
+  let url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + redirectUrl
+  + "&prompt=consent&response_type=code&client_id=" + clientId + "&scope=" + scope
+  + "&access_type=offline";
 
   //if the user has loged out, user should not be able to access this page
   const isLoggedIn = localStorage.getItem("token");
   const userType = localStorage.getItem("type");
+
   React.useEffect(() => {
+
+    console.log('dkdkdk', redirectUrl)
+    console.log('uri..', url)
+    
     if (!isLoggedIn && userType !== "manager") {
       toast.error("You must first login to view the messages", {
         position: "top-right",
@@ -48,6 +59,15 @@ const ManagerHome = () => {
     //re-dirrect to login pg
     Navigate('/login')
   }
+
+  const signInToGoogle = () => {
+
+    console.log('uri..', url)
+    
+    setTimeout(() => {
+      window.location = url;
+    }, 1000);
+  }
     
   return (
     <>
@@ -64,7 +84,18 @@ const ManagerHome = () => {
       {/* send messages */}
       <SendMessageBox />
       
-      <UploadDrive />
+      <div className="container">
+        <div className="g-signin-upload-container">
+          <div className="g-signin-border-container">
+          <img src="images/google-logo.png" width="30" height="30" alt="google-logo"/>
+
+            <Button className="btb btn-primary g-signin-btn" id="login" onClick={signInToGoogle}>
+              Sign In to Google to upload files
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* <UploadDrive /> */}
     </>
   );
 };
